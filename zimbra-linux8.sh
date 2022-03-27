26d7c17cf723a0bcad473a4a8758b1d392bff065
warning31 <mustafaavci33@gmail.com>
2022-03-27 18:52:21 +0300

    Update zimbra-linux8.sh

--- zimbra-linux8.sh
+++ zimbra-linux8.sh
@@ -102,8 +102,8 @@ NAMED=`ls /etc/ | grep named.conf.back`;
 	cp /etc/named.conf /etc/named.conf.back        
         fi
 
-sed -i s/"listen-on port 53 { 127.0.0.1; };"/"listen-on port 53 { 127.0.0.1; $IPADDRESS ; };"/g /etc/named.conf
-sed -i s/"allow-query     { localhost; };"/"allow-query     { localhost; $IPADDRESS ; };"/g /etc/named.conf
+sed -i s/"listen-on port 53 { 127.0.0.1; };"/"listen-on port 53 { 127.0.0.1; any ; };"/g /etc/named.conf
+sed -i s/"allow-query     { localhost; };"/"allow-query     { localhost; any ; };"/g /etc/named.conf
 
 echo 'zone "'$DOMAIN'" IN {' >> /etc/named.conf
 echo "        type master;" >> /etc/named.conf
